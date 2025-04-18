a0b4d6a4aba2f1133f0228f1ac23df3a5d1ec6d6
Ren-git-hub <renish6132@gmail.com>
2025-04-18 14:02:14 -0400

    security and JWT feature added

---
+++ src/main/java/com/renish/husband4hire_apis/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.renish.husband4hire_apis.config;
+
+import com.renish.husband4hire_apis.filters.JWTFilter;
+import com.renish.husband4hire_apis.service.JWTService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    JWTFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(
+                        request -> request
+                                .requestMatchers("user/login","/user/signup")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        authProvider.setUserDetailsService(userDetailsService);
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/com/renish/husband4hire_apis/service/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.renish.husband4hire_apis.service;
+
+import com.renish.husband4hire_apis.model.UserPrincipal;
+import com.renish.husband4hire_apis.model.Users;
+import com.renish.husband4hire_apis.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepo.findUsersByUsername(username);
+
+        if(user == null) {
+            System.out.println("user not found");
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new UserPrincipal(user);
+    }
+}
